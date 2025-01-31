12f5b3061dd29efa313a0a6b12f4a336e374a9c3
dokdo <132822988+onepythonlove@users.noreply.github.com>
2025-01-31 15:14:35 +0900

    ai 기능 안정화 및 버그개선

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 api_key = st.secrets["key"]
 
 
+
 #경고
 st.warning("dokdox 에서 만든 이 페이지는 개발중인 기능으로 아직 완성되지 않았습니다.오류가 발생할수도 있습니다.")
 # API 클라이언트 초기화
@@ -21,10 +22,9 @@ st.write("Dokdox is the world's most wonderful web service.")
 st.write("Thank you for using the free AI service!")
 
 # 사이드바에 슬라이더 추가
-
 # 응답 히스토리 초기화
-num_sentences = st.sidebar.slider("토큰범위설정-0으로 할 경우 오류가 발생할수도 있습니다.", min_value=0, max_value=500, value=50)
-
+num_sentences = st.sidebar.slider("토큰허용수", min_value=1, max_value=500, value=50)
+t = st.sidebar.slider("온화도", min_value=0, max_value=1, value=1)
 # 사용자 입력 받기
 user_input = st.chat_input("대화하려면 여기에 입력하세요.../Type here to chat!")
 # 사이드바에 슬라이더 추가
@@ -34,8 +34,10 @@ if user_input:
     # Cohere API 호출 시 문장 수 제한 추가
     response = co.chat(
         model="command-r7b-12-2024",
-        messages=[{'role': 'user', 'content': user_input + "사용자가 너한테 이름을 물어보면 너이름은 트로이아 야."}],
+        messages=[{'role': 'user', 'content': user_input }],
+        temperature=t,
         max_tokens=num_sentences
+
     )
     st.chat_message("AI").write(response.message.content[0].text)
     st.write("This AI response was generated using Cohere's language model. / Non-commercial use only.")
